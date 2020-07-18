c5e46630f6ac9d819c893132f3cd2b35e6e9b19e
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-07-18 01:44:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+function createEmployeeRecord(employeeInfo){
+  
+  return {
+    firstName: employeeInfo[0],
+    familyName: employeeInfo[1],
+    title: employeeInfo[2],
+    payPerHour: employeeInfo[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  
+}
+
+function createEmployeeRecords(employees){
+    let employeeRecords = employees.map(function(employee){
+      return createEmployeeRecord(employee)
+    })
+    return employeeRecords
+}
+
+function createTimeInEvent(employee, dateStamp){
+  console.log(typeof dateStamp.strip(" ")[1])
+ // employee.timeInEvents.push(dateStamp)
+}
